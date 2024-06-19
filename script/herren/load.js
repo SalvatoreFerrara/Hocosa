7573cf9f95f8b4e3a206e4ee20d6256cc39df24a
SalvatoreFerrara <salvatoreferrarabs@gmail.com>
2024-06-19 17:08:33 +0200

    fix herren footer and header

---
+++ script/herren/load.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('/../comm/herren/footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('foot').innerHTML = data;
+      });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('/../comm/herren/header.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('head').innerHTML = data;
+      });
+});
+
+
+// const hamburger = document.querySelector('.hamburger');
+// hamburger.addEventListener("click",function(){
+//   this.classList.toggle("close");
+// })
